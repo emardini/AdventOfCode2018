5872075bca192e8cf3e7870d18145f9e54102f4b
Eduardo Mardini <emardini@outsitenetworks.com>
2018-12-17 08:44:38 -0500

    Day 9, 10, 11

---
+++ Day_11_A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_11_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serial = 8979;
+            var range = Enumerable.Range(1, 300);
+            var cells = (from r1 in range
+                         from r2 in range
+                         select new { x = r1, y = r2 })
+                         .Select(x => new { x.x, x.y, power = ( ( ( (10 + x.x) * x.y + serial ) * ( 10 + x.x ) / 100 ) % 10 ) - 5   })
+                         .ToList();
+
+            var searchRange = Enumerable.Range(2, 299);
+            var searchCells = (from r1 in searchRange
+                               from r2 in searchRange
+                               select new { x = r1, y = r2 }).ToList();
+
+            var maxPower = searchCells.AsParallel().Select(s => new { s.x, s.y, totalpower = cells.Where(c => c.x >= s.x - 1 && c.x <= s.x + 1 && c.y >= s.y - 1 && c.y <= s.y + 1).Sum(z => z.power) }).OrderByDescending(x => x.totalpower).FirstOrDefault();
+            Console.WriteLine(maxPower);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Day_9_A/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_9_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MarbleGame().InsertMarble(6);
+        }
+
+        public class MarbleGame
+        {
+            int currentIndex = 0;
+            readonly List<int>  data = new List<int>() { 0 };
+            public int WinningScore(int nbPlayers, int nbMarble)
+            {
+                return 1;
+            }
+
+            public void InsertMarble(int value)
+            {
+                foreach (var index in Enumerable.Range(0, 10))
+                {
+                    currentIndex = GetNewCurrentIndex().GetValueOrDefault();
+                    data.Insert(currentIndex, index);
+                    Console.WriteLine(currentIndex);
+                }
+                Console.ReadKey();
+            }
+
+            public int? GetNewCurrentIndex()
+            {
+                if (data.Count == 0) return null;
+
+                if (data.Count == 1) return 1;
+
+                if (data.Count == 2) return 1;
+
+                if (((currentIndex + 1) / data.Count) > 0 && ((currentIndex + 1) % data.Count) == 0)
+                    return data.Count;
+
+                if (((currentIndex + 1) / data.Count) > 0 && ((currentIndex + 1) % data.Count) > 0)
+                    return 1;
+
+                return ((currentIndex + 2) % data.Count);
+            }
+        }
+    }
+}
